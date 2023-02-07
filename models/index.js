d1dbbfcc084daf2d93188dfd60a6d6d3ff9a8f02
Jackie <jjac.lee@gmail.com>
2023-02-07 12:57:50 -0800

    added a read me and video

--- models/index.js
+++ models/index.js
@@ -4,6 +4,10 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//associations that connects tables together
+//product and category connects with each other via category_id and categorie's primary ID
+
+//junction table created for product and tags using the product_tag table 
 
 Product.belongsTo(Category,{foreignKey:"category_id"});
 Category.hasMany(Product);
